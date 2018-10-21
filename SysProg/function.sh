3da3f097b0659c43a602e6dc8a6838f6616baf83
mashemat <mashemat@gmail.com>
2018-10-21 22:52:11 +0200

    First Commit

---
+++ SysProg/function.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+var_scope () {
+local gv1='local init'
+gv2='Changed inside'
+echo Inside function: gv1 is $gv1 : gv2 is $gv2
+}
+
+gv1='global init'
+gv2='global init'
+
+echo Before call: gv1 is $gv1 : gv2 is $gv2
+var_scope
+echo After call: gv1 is $gv1 : gv2 is $gv2
+
